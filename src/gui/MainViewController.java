c40fe98aa4e810be5b877f9d5ebefe7d371cd365
Gabriel <gabriel.moretto9090@gmail.com>
2022-08-11 17:16:23 -0300

    Página de departamentos

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -33,7 +33,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentsAction() {
-		System.out.println("onMenuItemDepartmentsAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
