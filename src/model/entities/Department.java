21916c9ee1c912238367859b8d1df02023dae0ac
Gabriel <gabriel.moretto9090@gmail.com>
2022-08-12 13:43:06 -0300

    DepartmentListController

---
+++ src/model/entities/Department.java
@@ -0,0 +1,22 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Department implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	
+	public Department() {}
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Integer getId() {return this.id;}
+	public void setId(Integer id) {this.id = id;}
+	
+	public String getName() {return this.name;}
+	public void setName(String name) {this.name = name;}
+}
